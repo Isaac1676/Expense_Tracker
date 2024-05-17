fd60d5dbded49c8c5857262378587e81883e6031
Isaac1676 <ezechielncho225@gmail.com>
2024-05-17 12:57:17 +0000

    finish app_name and icon

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,35 @@ import 'package:expense_tracker/database/database.dart';
 import 'package:expense_tracker/pages/home_page.dart';
 import 'package:expense_tracker/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterNativeSplash.preserve(
+      widgetsBinding: WidgetsFlutterBinding.ensureInitialized());
+  
+  await Future.delayed(const Duration(seconds: 2));
+
+  FlutterNativeSplash.remove();
+
   await ExpenseDatabase.initialize();
 
-  runApp(
-    MultiProvider(
-      providers: [
-        //theme provider
-        ChangeNotifierProvider(
-          create: (context) => ThemeProvider(),
-        ),
+  runApp(MultiProvider(
+    providers: [
+      //theme provider
+      ChangeNotifierProvider(
+        create: (context) => ThemeProvider(),
+      ),
 
-        //expense provider
-        ChangeNotifierProvider(
-          create: (context) => ExpenseDatabase(),
-        ),
-      ],
-      child: const MyApp(),
-    )
-  );
+      //expense provider
+      ChangeNotifierProvider(
+        create: (context) => ExpenseDatabase(),
+      ),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
